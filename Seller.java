09af1192e0071642332e3e454a1d84474edfdca5
Ryan Milem <gint271@cs.washington.edu>
2017-09-25 14:06:41 -0700

    Initial outline

---
+++ Seller.java
@@ -0,0 +1,20 @@
+
+// Main class, to be run at the start of mining.
+
+public class Seller {
+
+	public static void main(String[] args) {
+		// Record start time
+		
+		// Wait for end, get range and size
+		// TODO: Can these be gotten from log?
+		
+		// Read logs
+		
+		// Calculate best stations
+		
+		// Output to user
+
+	}
+
+}
